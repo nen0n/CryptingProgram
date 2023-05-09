ef98f1cafa79ed592e2f9e43df7a14e129855886
nen0n <gzdesenko38@gmail.com>
2023-05-09 20:47:46 +0300

    XOR rework

--- XOR.cs
+++ XOR.cs
@@ -8,23 +8,24 @@ namespace CryptingProgram
 {
     public class XOR : Crypting
     {
-        public override string CryptText(Crypt crypt, string plainText, string key)
+        public override string CryptText(Crypt crypt, string inputText, string key)
+        {
+            string gamma = GammaGenerator(key, inputText);
+            string outputText = "";
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                outputText += (char)(gamma[i] ^ inputText[i % inputText.Length]);
+            }
+            return outputText;
+        }
+
+        private string GammaGenerator(string key, string inputText)
         {
             byte[] keyBytes = Encoding.Unicode.GetBytes(key); // Переведення в байти ключа
             byte[] hashBytes = SHA256.Create().ComputeHash(keyBytes); // Хешування байтів
             string hashString = BitConverter.ToString(hashBytes).Replace("-", "").ToLower(); // Переведення в стрічку
-            string gamma = String.Concat(Enumerable.Repeat(hashString, 100)); // Повторення тексту 100 разів
-
-            byte[] byteKey = Encoding.Unicode.GetBytes(gamma);
-            byte[] inputBytes = Encoding.Unicode.GetBytes(plainText);
-            string output;
-
-            for (int i = 0; i < inputBytes.Length; i++)
-            {
-                inputBytes[i] ^= byteKey[i % byteKey.Length]; // XOR оператор
-            }
-            output = Encoding.Unicode.GetString(inputBytes);
-            return output;
+            string gamma = String.Concat(Enumerable.Repeat(hashString, inputText.Length / hashString.Length + 1));
+            return gamma;
         }
     }
 }
