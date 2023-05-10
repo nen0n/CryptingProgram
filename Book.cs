3bc923119a199b96fb57769a6331fa4b9b2ccddb
nen0n <gzdesenko38@gmail.com>
2023-05-10 03:05:03 +0300

    BookRework

--- Book.cs
+++ Book.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace CryptingProgram
 {
@@ -11,38 +12,36 @@ namespace CryptingProgram
         public override string CryptText(Crypt crypt, string inputText, string key)
         {
             string outputText = "";
-
             inputText = inputText.ToLower();
-
-            var matrix = MatrixGenerator(key);
-
+            var matrixOfVerse = MatrixGenerator(key);
             if (crypt == Crypt.Encrypt)
             {
                 Random random = new Random();
-                bool found;
+                bool foundRigthPair;
+                List<string> foundedPairs = new List<string> { };
                 foreach (char letter in inputText)
                 {
-                    found = false;
-                    int randX = random.Next(0, 9);
-                    int randY = random.Next(0, 9);
-                    for(int i = 0 + randX; i < 10 + randX; i++)
+                    foundedPairs.Clear();
+                    foundRigthPair = false;
+                    for (int i = 0; i < 10; i++)
                     {
-                        for(int j = 0 + randY; j < 10 + randY; j++)
+                        for(int j = 0; j < 10; j++)
                         {
-                            if (!found)
+                            if (letter == matrixOfVerse[i, j])
                             {
-                                if (letter == matrix[i % 10, j % 10])
-                                {
-                                    outputText += (i % 10 + 1).ToString() + "/" + (j % 10 + 1).ToString() + ", ";
-                                    found = true;
-                                }
+                                foundedPairs.Add((i + 1).ToString() + "/" + (j + 1).ToString() + ", ");
+                                foundRigthPair = true;
                             }
                         }
                     }
-                    if(found == false)
+                    if (foundRigthPair == false)
                     {
-                        outputText += (randX + 1).ToString() + "/" + (randY + 1).ToString() + ", ";
-                    }    
+                        outputText += random.Next(1, 11).ToString() + "/" + random.Next(1, 11).ToString() + ", ";
+                    }
+                    else
+                    {
+                        outputText += foundedPairs[random.Next(0, foundedPairs.Count)];
+                    }
                 }
             }
             if (crypt == Crypt.Decrypt)
@@ -53,7 +52,7 @@ namespace CryptingProgram
                     string[] twoCoordinates = pairs[pair].Split('/');
                     int i = int.Parse(twoCoordinates[0]);
                     int j = int.Parse(twoCoordinates[1]);
-                    outputText += matrix[i - 1, j - 1];
+                    outputText += matrixOfVerse[i - 1, j - 1];
                 }
             }
             return outputText;
@@ -61,21 +60,20 @@ namespace CryptingProgram
 
         private char[,] MatrixGenerator(string key)
         {
-            string keyToLower = key.ToLower();
-            string keyWithDash = keyToLower.Replace("\r\n", "I");
-            keyWithDash = keyWithDash.Replace("\n", "I");
-            string keyWithoutSpaceWithDash = String.Concat(keyWithDash.Where(letter => Char.IsLetter(letter)));
-            string[] substrings = keyWithoutSpaceWithDash.Split('I');
-            char[,] matrix = new char[10, 10];
-
+            char[,] matrixOfVerse = new char[10, 10];
+            string[] linesOfVerse = key.ToLower().Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            for(int line = 0; line < 10; line++)
+            {
+                linesOfVerse[line] = string.Concat(linesOfVerse[line].Where(letter => Char.IsLetter(letter)));
+            }
             for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    matrix[i, j] = substrings[i][j];
+                    matrixOfVerse[i, j] = linesOfVerse[i][j];
                 }
             }
-            return matrix;
+            return matrixOfVerse;
         }
     }
 }
